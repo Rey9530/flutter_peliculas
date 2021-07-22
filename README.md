04c012d7e7071f097c9bcbc1c899b4d6f6f8194f
Rey9530 <alex.rey.rosales.95@gmail.com>
2021-07-21 22:37:59 -0600

    app ya compilada

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# flutter_peliculas
+# apppelis
 
 A new Flutter project.
 
--- android/app/src/main/kotlin/com/reynaldo/rosales/apppelis/MainActivity.kt
+++ android/app/src/main/kotlin/com/reynaldo/rosales/apppelis/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_peliculas
+package com.reynaldo.rosales.apppelis
 
 import io.flutter.embedding.android.FlutterActivity
 
